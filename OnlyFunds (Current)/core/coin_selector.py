e38ceb1f2c1ff22ff1e36e2aeade4b5c930b63ad
Sgtbananas <jsmethurst94@gmail.com>
2025-06-05 19:46:26 +1000

    .

--- OnlyFunds (Current)/core/coin_selector.py
+++ OnlyFunds (Current)/core/coin_selector.py
@@ -83,7 +83,7 @@ def get_top_200_coinex_symbols():
 
     Returns: List of symbol strings (format: BTCUSDT, ETHUSDT, ...)
     """
-    url = "https://api.coinex.com/v2/spot/market/list-market"
+    url = "https://api.coinex.com/v1/market/list"
     fallback_symbols = ["BTCUSDT", "ETHUSDT"]
 
     try:
